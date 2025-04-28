87e12234dc88fb85955bc6a5c70175b52529b7db
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-04-28 18:18:32 +0300

    4 get articles

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const { getApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticleById } = require("./controllers/articles.controller");
+const { getArticleById, getArticles } = require("./controllers/articles.controller");
 
 const app = express();
 
@@ -11,6 +11,8 @@ app.get("/api", getApi);
 app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getArticles)
 
 app.all('/*splat', (req, res) => {
   res.status(404).send({ msg: "Invalid url!" });
