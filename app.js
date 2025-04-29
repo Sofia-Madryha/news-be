764e9d2ead66525d8673f5e1370d0124583b33ca
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-04-29 12:23:42 +0300

    5 - get comments by article id

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const { getApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticleById, getArticles } = require("./controllers/articles.controller");
+const { getArticleById, getArticles, getCommentsByArticleId } = require("./controllers/articles.controller");
 
 const app = express();
 
@@ -12,7 +12,9 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
 
-app.get("/api/articles", getArticles)
+app.get("/api/articles", getArticles);
+
+app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
 
 app.all('/*splat', (req, res) => {
   res.status(404).send({ msg: "Invalid url!" });
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,8 @@
-const { selectArticleById, selectArticles } = require("../models/articles.model");
+const {
+  selectArticleById,
+  selectArticles,
+  selectCommentsByArticleId,
+} = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const articleId = req.params.article_id;
@@ -14,6 +18,19 @@ exports.getArticles = (req, res, next) => {
   return selectArticles()
     .then((articles) => {
       res.status(200).send({ articles });
+    })
+    .catch(next);
+};
+
+exports.getCommentsByArticleId = (req, res, next) => {
+  const articleId = req.params.article_id;
+
+  const checkArticleId = selectArticleById(articleId);
+  const selectCommentsByArticle = selectCommentsByArticleId(articleId);
+
+  Promise.all([selectCommentsByArticle, checkArticleId])
+    .then((result) => {
+      res.status(200).send({ comments: result[0] });
     })
     .catch(next);
 };
